331d84336500b682ccca6ec11076b669683ff088
Bethsyf <bethsyfalcon@gmail.com>
2024-08-21 08:57:36 -0500

    update

--- ts-server/features/steps/context.ts
+++ ts-server/features/steps/context.ts
@@ -24,7 +24,7 @@ export type CreateExpense = {
   user: string;
   group: string;
   description: string;
-  value: string;
+  amount: string;
   participants: string[];
 };
 
@@ -170,7 +170,7 @@ export default class Context {
       groupId: groupId!,
       userId: userId!,
       description: expense.description,
-      value: expense.value,
+      value: expense.amount,
       participants: expense.participants.map((p) => {
         const id = this.users[this.replace(p)].result?.id;
         expect(id).toBeDefined();
